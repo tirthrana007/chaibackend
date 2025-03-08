ffcb498cb899775070b875b7508da100a077a23b
tirthrana007 <ranatirth7@gmail.com>
2025-03-08 15:36:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/instagram', (req, res) => {
+    res.send('_tirthh___')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
